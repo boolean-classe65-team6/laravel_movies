ac6cdc2ac030edb4e7ab2177754fc25c943b95d2
Marius L <mariusleica@gmail.com>
2022-08-22 16:51:06 +0200

    create

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// create
+Route::get("/movies/create", "MovieController@create")->name("movies.create");
