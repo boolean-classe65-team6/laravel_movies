09808b5dfd00c46bfae611ef6440fe5ef6a7cbe9
Marius L <mariusleica@gmail.com>
2022-08-22 17:41:29 +0200

    problemy layout

---
+++ app/Http/Controllers/Admin/MovieController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMovieRequest;
+use App\Http\Requests\UpdateMovieRequest;
+use App\Movie;
+use App\Traits\MovieRating;
+
+class MovieController extends Controller
+{
+    use MovieRating;
+
+    public static $movieValidationRules = [
+        "title" => "required",
+        "overview" => "required",
+        "release_date" => "nullable|date",
+        "poster_img" => "required",
+        "running_time" => "nullable|integer|min:10",
+        "parental_rating" => "nullable",
+        "original_language" => "nullable|size:2",
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // recuperiamo tutti i dati senza poter indicare l'ordine
+        // Di default l'ordine è per id
+        // $movies = Movie::all();
+
+        // Recuperiamo tutti i dati in un ordine desiderato
+        $movies = Movie::orderBy("title", "asc")->get();
+
+        return view("admin.movies.index", compact("movies"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("admin.movies.create", [
+            "movieRatings" => $this->movieRatings
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMovieRequest $request)
+    {
+        $data = $request->validated();
+
+        $movie = new Movie();
+        $movie->fill($data);
+        $movie->save();
+
+        return redirect()->route("admin.movies.show", $movie->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        // operazione eseguita dal findOrFail e anche dalla Dependecy Injection
+        /* $movie = Movie::find($id);
+        if (!$movie) {
+            abort(404);
+        } */
+
+        return view("admin.movies.show", compact("movie"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $movie)
+    {
+        return view(
+            "admin.movies.edit",
+            [
+                "movieRatings" => $this->movieRatings,
+                "movie" => $movie
+            ]
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMovieRequest $request, Movie $movie)
+    {
+        $data = $request->validated();
+
+        $movie->update($data);
+
+        return redirect()->route("admin.movies.show", $movie->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "HomeController@index")->name("home");
 
+Route::middleware("auth")
+    ->prefix("admin")
+    ->name("admin.")
+    ->namespace("Admin")
+    ->group(function () {
+
+        Route::get('/', 'HomeController@index')->name('home');
+
+
+        // index
+        Route::get("/movies", "MovieController@index")->name("movies.index");
+
+
+        // create
+        Route::get("/movies/create", "MovieController@create")->name("movies.create");
+        // store
+        Route::post("/movies", "MovieController@store")->name("movies.store");
+
+
+        // show
+        Route::get("/movies/{movie}", "MovieController@show")->name("movies.show");
+
+
+        // edit - mostra una view con il form della risorsa
+        Route::get("/movies/{movie}/edit", "MovieController@edit")->name("movies.edit");
+        // update - riceve i dati dal form e li salva a db
+
+        // Route::patch("/movies/{movie}", "MovieController@update")->name("movies.update");
+        // Route::put("/movies/{movie}", "MovieController@update")->name("movies.update");
+
+        // tramite il match indichiamo che la rotta può essere chiamata sia in PATCH che in PUT
+        Route::match(["patch", "put"], "/movies/{movie}", "MovieController@update")->name("movies.update");
+
+        // destroy
+        Route::delete("/movies/{movie}", "MovieController@destroy")->name("movies.destroy");
+    });
+
+
+
+// Crea tutte le rotte necessarie per l'autenticazione: Login, reset password, register, email verify
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-// create
-Route::get("/movies/create", "MovieController@create")->name("movies.create");
